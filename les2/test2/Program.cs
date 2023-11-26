a8798104bf0348aaebc5b2e5571be94ba3b5904e
lobutev Sergey <lobutev.serega@mail.ru>
2023-11-26 19:13:41 +0300

    ураX3

---
+++ les2/test2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+class Program
+{
+    static void Main()
+    {
+         int[] array = {10,1,2,33,4,5,61,71,80};
+         int X = 0;
+         foreach (int item in array)
+         {
+            int remains = item % 2;
+            if (remains == 0)
+                {
+                X += 1;
+                }
+         }
+         
+        Console.WriteLine(X);
+    }
+}      
---
+++ les2/test3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+class Program
+{
+    static void Main()
+    {
+        float[] array = {10.4f,1.4f,2.1f,33.1f,4.1f,5.1f,61.5f,71.5f,80.1f};
+        float max = array.Max();
+        float min = array.Min();
+    Console.WriteLine(max - min);
+    }
+}      
