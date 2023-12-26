b5f5b2a9250ff40710a8c47c2240aa774c6031cd
lobutev Sergey <lobutev.serega@mail.ru>
2023-12-26 21:50:50 +0300

    last
    last

---
+++ itog att/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ввод исходного массива
+            Console.WriteLine("Введите исходный массив строк:");
+            string[] inputArray = Console.ReadLine().Split(' ');
+            
+            // Формирование нового массива
+            string[] outputArray = new string[0];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i].Length <= 3)
+                {
+                    outputArray = outputArray.Append(inputArray[i]).ToArray();
+                }
+            }
+
+            // Вывод нового массива
+            Console.WriteLine("Новый массив:");
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                Console.WriteLine(outputArray[i]);
+            }
+        }
+    }
+}
